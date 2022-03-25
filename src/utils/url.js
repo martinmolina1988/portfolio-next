0349b73e5e4b537953c3c5c9f3dace497e6b93bf
martinmolina1988 <67392381+martinmolina1988@users.noreply.github.com>
2022-03-24 21:26:23 -0300

    fix API_URL

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,2 +1,6 @@
-export const API_URL = 'http://localhost:3000'
-export const API_URL_PRODUCTION = 'https://martinmolina1988.netlify.app'
+
+const url = {
+  API_URL_LOCAL: 'http://localhost:3000',
+  API_URL_PRODUCTION: 'https://martinmolina1988.netlify.app'
+}
+export const API_URL = url.API_URL_PRODUCTION
